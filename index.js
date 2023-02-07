1ee8ea49ae170610fe091639f2d722d62141338a
Benjamin Monk <Bmonk1996@yahoo.com>
2023-02-07 11:00:25 -0600

    check wins for column and rows

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const myBoard = document.getElementById('board')
 
 const gameState = {
     board: [],
-    boardSize:3,
+    winNum:3,
     pTwoHuman:null,
     letters:['X','O'],
     playerOneTurn:true
@@ -20,30 +20,68 @@ function playerMove(isPlayerOne,mySquare){
     }
 }
 //updates the board in JS
-function updateBoard(letter, squareNumber){
-    let row =  Math.floor(squareNumber / gameState.board[0].length)
-    let column = squareNumber % gameState.board[0].length
-    console.log(row, gameState.board[0].length)
-    console.log(column, gameState.board.length)
+function updateBoard(letter, row, column){
     gameState.board[row][column] = letter
-    console.log(gameState.board)
 }
 //checks if the move wins the game
-function checkWin(letter, squareNumber){
+function checkWin(letter, row, column){
+    let count = 0
+    //check column
+    for(let i = 0; i< gameState.board.length; i++){
+        if(gameState.board[i][column] === letter){
+            count++
+        }else{
+            count = 0
+        }
+        if(count === gameState.winNum){
+            return true
+        }
 
+    }
+    count = 0
+    //check row
+    for(let i = 0; i< gameState.board[0].length; i++){
+        if(gameState.board[row][i] === letter){
+            count++
+            console.log(count)
+        }else{
+            count = 0
+        }
+        if(count === gameState.winNum){
+            return true
+        }
+    }
+    count = 0
+    //check left to rightdiagonal
+    
+    return false
 }
 
+function isValidSquare(row, column){
+return  true
+}
 //when a player clicks on a square
 function selectSquare(event){
     const mySquare = event.target
-    squareNumber = mySquare.id
-    letter = playerMove(gameState.playerOneTurn,mySquare)
-    updateBoard(letter,squareNumber)
-    let win = checkWin(letter, squareNumber)
+
+    if(mySquare.matches('td')){
+
+        squareNumber = mySquare.id
+        let row =  Math.floor(squareNumber / gameState.board[0].length)
+    let column = squareNumber % gameState.board[0].length
+        if(isValidSquare(row, column)){
+            letter = playerMove(gameState.playerOneTurn,mySquare)
+            updateBoard(letter,row, column)
+            let win = checkWin(letter, row, column)
+             console.log(win) 
+        }else{
+            //invalid square
+        }
+    }
+
 }
 //creates the board in JS and html
-function createBoard(rows, columns){
-    
+function createBoard(rows, columns){    
     let squareCount = 0
     for(let i = 0; i < rows; i++){
         const newHTMLRow = document.createElement('tr')
@@ -51,6 +89,7 @@ function createBoard(rows, columns){
         for(let j = 0; j < columns; j++){
             const newHTMLSquare = document.createElement('td')
             newHTMLSquare.innerText = squareCount
+            newHTMLSquare.id = squareCount
             if(j > 0 && j < columns - 1){
                 newHTMLSquare.classList.add('inner-column')
             }
