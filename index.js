995be8cc26d02e3ff368a956251849f46b3dc869
Benjamin Monk <Bmonk1996@yahoo.com>
2023-02-07 09:25:47 -0600

    select square function works

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 const myTable = document.getElementsByTagName('table')[0]
 
 
+const gameState = {
+    board: [[null, null, null],
+            [null, null, null],
+            [null, null, null]],
+    boardSize:3,
+    pTwoHuman:null,
+    letters:['X','O'],
+    playerOneTurn:true
+}
+
+function playerMove(isPlayerOne,mySquare){
+    if(isPlayerOne){
+        mySquare.innerText = gameState.letters[0]
+        gameState.playerOneTurn = false
+    }else{
+        mySquare.innerText = gameState.letters[1]
+        gameState.playerOneTurn = true
+    }
+    return letter
 
 
+}
 
-
-
+function checkWin(){
+}
 
 function selectSquare(event){
     const mySquare = event.target
-    console.log(mySquare)
+    squareNumber = mySquare.id
+    letter = playerMove(gameState.playerOneTurn,mySquare)
+
+    checkWin(letter, squareNumber)
 }
 
 
